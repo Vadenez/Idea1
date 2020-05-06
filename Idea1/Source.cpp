61638f33e37a220c78d73e6bc1c9f83286906390
Joseph Lavoie <63205161+Vadenez@users.noreply.github.com>
2020-05-06 16:46:06 -0600

    Made it start to work

--- Idea1/Source.cpp
+++ Idea1/Source.cpp
@@ -1,21 +1,31 @@
 #include "Header.h"
 
 int main() {
-    string ans, get;
-    int o;
-    string Y = "yes";
-    string N = "no";
-    this_thread::sleep_for(chrono::seconds(2));
+
+    string ans, get, Y, N, ans2, YN;
+
+    YN = "[Yes/No]";
+    Y = "yes";
+    N = "no";
+
     cout << "Would you like to create a text file (.txt)?" << endl;
-    cout << "[Yes/No]" << endl;
+    cout << YN << endl;
+
+    sleep_for(seconds(2));
+
     get = in(ans);
-    cout << ans << endl;
-    o = nonsens(Y, get);
-    cout << o;
+
     //Checks if ans is "yes"
-    while (o == 1) {
+    while (nonsens(Y, get) != 1) {
+
+        cout << "You said '" << get << "'" << endl;
 
         if (nonsens(N, get) == 1) {
+            
+            cout << "Are you sure you want the program to end?" << endl;
+            cout << YN << endl;
+
+            get = in(ans2);
 
             //Ends program
             cout << "Since you don't want to create a text file, the program will close now" << endl;
@@ -24,11 +34,12 @@ int main() {
                 return 0;
         }
         else {
-            cout << "You typed '" << get << "'," << endl << " Unfortunatly thats not 'Yes' or 'No'" << endl;
+            cout << " Unfortunatly thats not 'Yes' or 'No'" << endl;
 
             cout << "Under Construction (asks to try again, or exits)" << endl;
         }
     }
+    cout << "good" << endl;
     return 0;
     /*ofstream file;
     file.open("codebind.txt");
